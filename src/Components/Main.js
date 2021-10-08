60f745f3604503e672d6b55e4cedb6d1ff1f59a9
James Laugerman <jameslaugerman@gmail.com>
2021-10-07 19:28:21 -0500

    bareBones

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,71 +1,207 @@
-import React, { Component } from "react";
+import React from "react";
 import { WRData } from "../Data/FinalV2/WR";
 import { RBData } from "../Data/FinalV2/RB";
 import { QBData } from "../Data/FinalV2/QB";
 import { TEData } from "../Data/FinalV2/TE";
 import { Header } from "./Header";
 import { Switch, Route } from "react-router-dom";
-import { PlayerTable } from "./playerTable"
+import { useState } from "react";
+import MUIDataTable from "mui-datatables";
 
-const [RB, setRB] = useState(RBData);
-const [WR, setWR] = useState(WRData);
-const [QB, setQB] = useState(QBData);
-const [TE, setTE] = useState(TEDataWR);
+const PlayerTable = (props) => {
+	return (
+		<div>
+			<div className="container-fluid">
+				<MUIDataTable
+					title={props.title}
+					data={props.data}
+					columns={props.columns}
+					options={props.options}
+				/>
+			</div>
+		</div>
+	);
+};
 
-const mapValues = (position => position.map(Object.values));
 
-mapValues(QB);
-mapValues(TE);
-mapValues(WR);
-mapValues(RB);
 
-const slicer = (position) => position.forEach((a) => (a.Last_Name = a.Last_Name.slice(0, -1)));
+export const Main = () => {
+	const [RB, setRB] = useState(RBData);
+	const [WR, setWR] = useState(WRData);
+	const [QB, setQB] = useState(QBData);
+	const [TE, setTE] = useState(TEData);
 
-slicer(RB);
-slicer(QB);
-slicer(WR);
-slicer(TE);
+	const mapValues = (position) => position.map(Object.values);
 
-export class Main extends Component {
-	constructor(props) {
-		super(props);
+	mapValues(QB);
+	mapValues(TE);
+	mapValues(WR);
+	mapValues(RB);
 
-		this.state = {
-			RBData: RBData,
-			WRData: WRData,
-			QBData: QBData,
-			TEData: TEData,
-		};
-	}
-	render() {
-		const WRComponent = () => {
-			return <PlayerTable data={this.state.WRData} title={"Wide Receivers"} />;
-		};
+	// const slicer = (position) =>
+	// 	position.forEach((a) => (a.Last_Name = a.Last_Name.slice(0, -1)));
 
-		const RBComponent = () => {
-			return <PlayerTable data={this.state.RBData} title={"Running Backs"} />;
-		};
+	// slicer(RB);
+	// slicer(QB);
+	// slicer(WR);
+	// slicer(TE);
 
-		const QBComponent = () => {
-			return <PlayerTable data={this.state.QBData} title={"Quarter Backs"} />;
-		};
+	const columns = [
+		{ name: "Overall_Rank", label: "Overall Rank" },
+		{ name: "Position_Rank", label: "Position Rank" },
+		{ name: "First_Name", label: "First Name" },
+		{ name: "Last_Name", label: "Last Name" },
+		{ name: "Team" },
+		{ name: "Week1Projection", label: "Projected" },
+		{ name: "POR", label: "P.A.R." },
+		{ name: "JamesVal", label: "My Value" },
+		{ name: "300_Value", label: "ESPN Value" },
+		{ name: "Rookie\r", label: "Rookie" },
+	];
 
-		const TEComponent = () => {
-			return <PlayerTable data={this.state.TEData} title={"Tight Ends"} />;
-		};
+	const options = {
+		selectableRows: true,
+		selectableRowsOnClick: true,
+		onRowsDelete: (e) => {
+			// console.log(e.data[0].index);
+			console.log(e.data);
+		},
+		selectedRows: {
+			delete: "Delete",
+			deleteAria: "Delete Selected Rows",
+		},
+	};
 
+	const WRoptions = {
+		selectableRows: "multiple",
+		selectableRowsOnClick: true,
+		onRowsDelete: (e) => {
+			const dataIndex = e.data[0].dataIndex;
+			const playerToDelete = WR[dataIndex];
+			const playerRank = playerToDelete.Overall_Rank;
+	
+			const filteredWR = WR.filter((a) => a.Overall_Rank !== playerRank);
+	
+			setWR(filteredWR);
+		},
+		selectedRows: {
+			delete: "Delete",
+			deleteAria: "Delete Selected Rows",
+		},
+	};
+
+	const RBoptions = {
+		selectableRows: "multiple",
+		selectableRowsOnClick: true,
+		onRowsDelete: (e) => {
+			const dataIndex = e.data[0].dataIndex;
+			const playerToDelete = RB[dataIndex];
+			const playerRank = playerToDelete.Overall_Rank;
+	
+			const filteredWR = RB.filter((a) => a.Overall_Rank !== playerRank);
+	
+			setRB(filteredWR);
+		},
+		selectedRows: {
+			delete: "Delete",
+			deleteAria: "Delete Selected Rows",
+		},
+	};
+
+	const TEoptions = {
+		selectableRows: "multiple",
+		selectableRowsOnClick: true,
+		onRowsDelete: (e) => {
+			const dataIndex = e.data[0].dataIndex;
+			const playerToDelete = TE[dataIndex];
+			const playerRank = playerToDelete.Overall_Rank;
+	
+			const filteredWR = TE.filter((a) => a.Overall_Rank !== playerRank);
+	
+			setTE(filteredWR);
+		},
+		selectedRows: {
+			delete: "Delete",
+			deleteAria: "Delete Selected Rows",
+		},
+	};
+
+	const QBoptions = {
+		selectableRows: "multiple",
+		selectableRowsOnClick: true,
+		onRowsDelete: (e) => {
+			const dataIndex = e.data[0].dataIndex;
+			const playerToDelete = QB[dataIndex];
+			const playerRank = playerToDelete.Overall_Rank;
+	
+			const filteredWR = QB.filter((a) => a.Overall_Rank !== playerRank);
+	
+			setQB(filteredWR);
+		},
+		selectedRows: {
+			delete: "Delete",
+			deleteAria: "Delete Selected Rows",
+		},
+	};
+
+	const WRComponent = () => {
 		return (
-			<>
-				<Header />
-				<Switch>
-					<Route path="/WR" component={WRComponent} />
-                    <Route path="/RB" component={RBComponent} />
-                    <Route path="/QB" component={QBComponent} />
-                    <Route path="/TE" component={TEComponent} />
-                    <Route path="/" component={RBComponent} />
-				</Switch>
-			</>
+			<PlayerTable
+				options={WRoptions}
+				columns={columns}
+				onDelete={setWR}
+				data={WR}
+				title={"Wide Receivers"}
+			/>
 		);
-	}
-}
+	};
 
+	const RBComponent = () => {
+		return (
+			<PlayerTable
+				options={RBoptions}
+				columns={columns}
+				onDelete={setRB}
+				data={RB}
+				title={"Running Backs"}
+			/>
+		);
+	};
+
+	const QBComponent = () => {
+		return (
+			<PlayerTable
+				options={QBoptions}
+				columns={columns}
+				onDelete={setQB}
+				data={QB}
+				title={"Quarter Backs"}
+			/>
+		);
+	};
+
+	const TEComponent = () => {
+		return (
+			<PlayerTable
+				options={TEoptions}
+				columns={columns}
+				onDelete={setTE}
+				data={TE}
+				title={"Tight Ends"}
+			/>
+		);
+	};
+
+	return (
+		<>
+			<Header />
+			<Switch>
+				<Route path="/WR" component={WRComponent} />
+				<Route path="/RB" component={RBComponent} />
+				<Route path="/QB" component={QBComponent} />
+				<Route path="/TE" component={TEComponent} />
+				<Route path="/" component={RBComponent} />
+			</Switch>
+		</>
+	);
+};
